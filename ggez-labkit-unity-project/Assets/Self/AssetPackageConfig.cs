5020fffe4210db8825bbae9eb46d077e8dd055f5
Karl Gluck <karlgluck@gmail.com>
2017-09-23 14:26:39 -0700

    labkit setup

---
+++ ggez-labkit-unity-project/Assets/Self/AssetPackageConfig.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace GGEZ
+{
+[CreateAssetMenu(fileName = "New Asset Package.asset", menuName="Asset Package Config")]
+public class AssetPackageConfig : ScriptableObject
+{
+public string AssetPackageName
+	{
+	get { return this.name; }
+	set { this.name = value; }
+	}
+
+public Object[] Assets;
+
+public string[] CollectAssetPaths ()
+	{
+	Object[] collectedAssets = this.Assets.Where (assetObject => assetObject != null).ToArray ();
+	for (int i = 0; i < 100; ++i)
+		{
+		var expanded = collectedAssets
+				.SelectMany (
+						delegate (Object assetObject)
+							{
+							if (assetObject is AssetPackageConfig)
+								{
+								return (assetObject as AssetPackageConfig).Assets;
+								}
+							else
+								{
+								return new Object[] {assetObject};
+								}
+							}
+						);
+		bool canReturn = !expanded.Any (assetObject => assetObject is AssetPackageConfig);
+		if (canReturn)
+			{
+			return expanded.Select (o => AssetDatabase.GetAssetPath (o)).ToArray ();
+			}
+		collectedAssets = expanded.ToArray ();
+		}
+	Debug.LogErrorFormat ("Too many iteration attempts while expanding asset references. Is there a recursive AssetPackageConfig reference?");
+	return null;
+	}
+
+public void Build ()
+	{
+	string targetDirectory = System.IO.Path.Combine ("../packages/", Application.unityVersion);
+	System.IO.Directory.CreateDirectory (targetDirectory);
+	var packagePath = System.IO.Path.Combine (targetDirectory, this.AssetPackageName + ".unitypackage");
+	try
+		{
+		AssetDatabase.ExportPackage (
+				this.CollectAssetPaths (),
+				packagePath,
+				ExportPackageOptions.Default
+				);
+		Debug.LogFormat ("Built {0}", System.IO.Path.GetFullPath (packagePath));
+		}
+	catch (System.Exception e)
+		{
+		Debug.LogErrorFormat ("BUILD FAILED\n{0}", e);
+		}
+	}
+}	
+}
---
+++ ggez-labkit-unity-project/Assets/Self/Editor/AssetPackageConfigEditor.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace GGEZ
+{
+[CustomEditor (typeof(AssetPackageConfig))]
+public class AssetPackageConfigEditor : Editor
+{
+public override void OnInspectorGUI()
+	{
+	var t = this.target as AssetPackageConfig;
+	t.AssetPackageName = EditorGUILayout.TextField ("Name", t.AssetPackageName);
+	EditorGUILayout.Space ();
+	EditorGUI.BeginDisabledGroup (t.Assets == null || t.Assets.Length < 1);
+	if (GUILayout.Button ("Build"))
+		{
+		t.Build ();
+		}
+	EditorGUI.EndDisabledGroup ();
+	EditorGUILayout.Space ();
+	GUILayout.Label ("Assets ", EditorStyles.boldLabel);
+	EditorGUILayout.Space ();
+    if (t.Assets == null)
+        {
+        t.Assets = new Object[0];
+        }
+	for (int i = t.Assets.Length - 1; i >= 0; --i)
+		{
+		var assetObject = t.Assets[i];
+		bool shouldDelete = assetObject == null;
+		if (!shouldDelete)
+			{
+			EditorGUILayout.BeginHorizontal ();
+			shouldDelete = GUILayout.Button ("x", EditorStyles.miniButton, GUILayout.Width(17f));
+			bool isAnotherAssetPackage = assetObject as AssetPackageConfig != null;
+			Color oldBackgroundColor = GUI.backgroundColor;
+			if (isAnotherAssetPackage)
+				{
+				GUI.backgroundColor = Color.green;
+				}
+			assetObject = EditorGUILayout.ObjectField (GUIContent.none, assetObject, typeof(Object), false);
+			GUI.backgroundColor = oldBackgroundColor;
+			shouldDelete = shouldDelete || assetObject == null;
+			EditorGUILayout.EndHorizontal ();
+			}
+		if (shouldDelete)
+			{
+			for (int a = i; a < t.Assets.Length - 1; ++a)
+				{
+				t.Assets[a] = t.Assets[a + 1];
+				}
+			System.Array.Resize(ref t.Assets, t.Assets.Length - 1);
+			}
+		else
+			{
+			t.Assets[i] = assetObject;
+			}
+		}
+	EditorGUILayout.Space ();
+	var newAssets = AssetPackageConfigEditor.dropAssetsArea ("Drop New Assets Here", "Add {0} Asset(s)");
+	if (newAssets != null)
+		{
+		int oldLength = t.Assets.Length;
+		System.Array.Resize (ref t.Assets, oldLength + newAssets.Length);
+		newAssets.CopyTo (t.Assets, oldLength);
+		}
+
+	}
+
+
+private static Object[] dropAssetsArea (string idlePrompt, string dropPrompt)
+    {
+	Event e = Event.current;
+	Rect drop_area = GUILayoutUtility.GetRect (0.0f, 50.0f, GUILayout.ExpandWidth (true));
+	bool isDragPerformEvent = e.type == EventType.DragPerform;
+	bool isAnyDragEvent = e.type == EventType.DragUpdated || isDragPerformEvent;
+	bool isMouseInArea = drop_area.Contains (e.mousePosition);
+	bool dragAndDropHasSomething = DragAndDrop.objectReferences != null && DragAndDrop.objectReferences.Length > 0;
+
+    var centeredStyle = GUI.skin.GetStyle("Box");
+    centeredStyle.alignment = TextAnchor.MiddleCenter;
+	GUI.Box (
+			drop_area,
+			isMouseInArea && dragAndDropHasSomething
+					? string.Format (dropPrompt, DragAndDrop.objectReferences.Length)
+					: idlePrompt,
+			centeredStyle
+			);
+
+	if (isAnyDragEvent && isMouseInArea)
+		{
+		DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+		if (isDragPerformEvent)
+			{
+			DragAndDrop.AcceptDrag();
+			return DragAndDrop.objectReferences;
+			}
+		}
+	
+	return null;
+    }
+}
+}
+
