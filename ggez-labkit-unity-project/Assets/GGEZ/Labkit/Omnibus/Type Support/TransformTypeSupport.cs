79560a083658d9afa77a8bef5f047d0ae1ad47e6
Karl Gluck <kgluck@riotgames.com>
2017-12-07 15:40:00 -0800

    Rect, RectTransform, Transform type support

---
+++ ggez-labkit-unity-project/Assets/GGEZ/Labkit/Omnibus/Type Support/TransformTypeSupport.cs
@@ -0,0 +1,58 @@
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// For more information, please refer to <http://unlicense.org/>
+
+using System;
+using UnityEngine;
+
+namespace GGEZ
+{
+namespace Omnibus
+{
+
+
+
+public sealed partial class Bus
+{
+
+public void SetTransform (string key, Transform value) { this.SetObject (key, value); }
+public void SignalTransform (string key, Transform value) { this.SignalObject (key, value); }
+public bool GetTransform (string key, out Transform value) { return this.getT<Transform> (key, out value); }
+public Transform GetTransform (string key, Transform defaultValue) { return this.getT<Transform> (key, defaultValue); }
+
+}
+
+
+public sealed partial class SerializedMemoryCell
+{
+
+public Transform Value_Transform;
+
+}
+
+
+
+}
+
+}
